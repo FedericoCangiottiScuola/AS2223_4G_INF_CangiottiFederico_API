c8bf80c2312cf99ef441a7d87836a2ca69337aeb
Federico Cangiotti <13657090-federicocangiotti@users.noreply.gitlab.com>
2023-03-16 08:13:29 +0100

    feat: Add Math model

---
+++ AS2223_4G_INF_CangiottiFederico_API/Math.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AS2223_4G_INF_CangiottiFederico_API
+{
+    public class Math
+    {
+        public enum Status
+        {
+            OK,
+            ERROR,
+            WARNING
+        }
+
+        public class Result
+        {
+            // converts enumeration values to and from strings
+            [JsonConverter(typeof(JsonStringEnumConverter))]
+            public Status Status { get; set; }
+            public string Message { get; set; }
+
+            public Result(Status status, string message = "")
+            {
+                this.Status = status;
+                this.Message = message;
+            }
+        }
+    }
+}
