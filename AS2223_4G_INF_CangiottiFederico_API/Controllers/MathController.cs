8cdcfe5c815c4919e3b2cb16916a7030b6f08d6d
Federico Cangiotti <13657090-federicocangiotti@users.noreply.gitlab.com>
2023-03-16 08:17:59 +0100

    feat: Add PariDispari method

--- AS2223_4G_INF_CangiottiFederico_API/Controllers/MathController.cs
+++ AS2223_4G_INF_CangiottiFederico_API/Controllers/MathController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using static AS2223_4G_INF_CangiottiFederico_API.Math;
 
 namespace AS2223_4G_INF_CangiottiFederico_API.Controllers
 {
@@ -7,6 +8,16 @@ namespace AS2223_4G_INF_CangiottiFederico_API.Controllers
     [Route("api/v1/")]
     public class MathController : Controller
     {
-
+        [HttpGet("GetAddition")]
+        public JsonResult PariDispari(int input)
+        {
+            Result outcome = new(Status.OK);
+            
+            return Json(new
+            {
+                result = input%2==0 ? "pari" : "dispari",
+                outcome,
+            });
+        }
     }
 }
