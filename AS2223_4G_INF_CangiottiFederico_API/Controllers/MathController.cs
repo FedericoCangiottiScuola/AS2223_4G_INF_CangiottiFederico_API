811400bfc29e892d7fb3c7274e7b82be9904452c
Federico Cangiotti <13657090-federicocangiotti@users.noreply.gitlab.com>
2023-03-16 08:11:32 +0100

    feat: Add Math controller decorators

--- AS2223_4G_INF_CangiottiFederico_API/Controllers/MathController.cs
+++ AS2223_4G_INF_CangiottiFederico_API/Controllers/MathController.cs
@@ -2,6 +2,9 @@
 
 namespace AS2223_4G_INF_CangiottiFederico_API.Controllers
 {
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/v1/")]
     public class MathController : Controller
     {
 
